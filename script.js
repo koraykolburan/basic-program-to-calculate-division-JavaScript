9401492cfd064359eb30b56e30b18440c006d145
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2021-09-28 13:28:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function divideBy() {
+  num1 = document.getElementById("number1").value;
+  num2 = document.getElementById("number2").value;
+  document.getElementById("result").innerHTML = num1 / num2;
+}
